e2a9e955b92e1c2c4a96039605140b18e7cfc44e
kokoro <128305501+dev090613@users.noreply.github.com>
2024-12-05 01:47:44 +0900

    done project 7

---
+++ 7/VMTranslator.py
@@ -0,0 +1,49 @@
+from parser import Parser
+from codewriter import CodeWriter
+
+class VMTranslator:
+    def __init__(self, input_file):
+        self.input_file = input_file
+        output_file = input_file.replace(".vm", ".asm")
+
+        self.parser = Parser(input_file)
+        self.code_writer = CodeWriter(output_file)
+
+        file_name = input_file.split("/")[-1].replace(".vm", "")
+        self.code_writer.setFileName(file_name)
+
+    def translate(self):
+
+        while self.parser.hasMoreCommands():
+            self.parser.advance()
+            command_type = self.parser.commandType()
+
+            if command_type == "C_ARITHMETIC":
+                self.code_writer.writeArithmetic(
+                        self.parser.arg1()
+                        )
+            elif command_type in ["C_PUSH", "C_POP"]:
+                self.code_writer.writePushPop(
+                        command_type,
+                        self.parser.arg1(),
+                        int(self.parser.arg2())
+                        )
+        self.code_writer.file.close()
+
+def main():
+    import sys
+    
+    if len(sys.argv) != 2:
+        print("Usage: python3 VMTranslator.py input_file.vm\n")
+        sys.exit(1)
+
+    input_file = sys.argv[1]
+    vm_translator = VMTranslator(input_file)
+    vm_translator.translate()
+    print("\nprogram complete.\n")
+
+
+
+
+if __name__ == "__main__":
+    main()
--- 7/codewriter.py
+++ 7/codewriter.py
@@ -2,80 +2,102 @@ class CodeWriter:
     def __init__(self, output_file: str) -> None:
 
         self.file = open(output_file, "w")
-            self.current_file = None
-            self.label_counter = 0
+        self.current_file = None
+        self.label_counter = 0
 
-        def setFileName(file_name: str) -> None:
-            self.current_file = file_name
-            return None
+        self.binary_op = { 
+                "add": "M=M+D", 
+                "sub": "M=M-D",
+                "and": "M=M&D", 
+                "or": "M=M|D"
+                }
+        self.segment_table = {
+                "local": "LCL",
+                "argument": "ARG",
+                "this": "THIS",
+                "that": "THAT",
+                "temp": "5", # RAM[5..12]
+                "pointer": "3", # RAM[3..4]
+                }
 
-        def writeArithmetic(command) -> None:
-            """
-            Binary: M=M+D, M=M-D, M&D, M|D
-            Unary: -M, !M
-            Comparison
-            """
-            binary_op = { 
-                    "add": "M=M+D", 
-                    "sub": "M=M-D",
-                    "and": "M&D", 
-                    "or": "M|D"
-                    }
-            if command in binary_op:
-                self._writeBinaryOperation(binary_op[command])
+    def __enter__(self):
+        '''
+        with -> enter to context
+        '''
+        return self
+    
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        """
+        out of with bolck -> resource retrieve
+        """
+        if self.file:
+            self.file.close()
 
-            elif command == "neg":
-                self._writeUnaryOperation("M=-M")
+    def setFileName(self, file_name: str) -> None:
+        self.current_file = file_name
+        return
 
-            elif command == "not":
-                self._writeUniaryOperation("M=!M")
+    def writeArithmetic(self, command) -> None:
+        """
+        Binary: M=M+D, M=M-D, M&D, M|D
+        Unary: -M, !M
+        Comparison
+        """
+        if command in self.binary_op:
+            self._writeBinaryOperation(self.binary_op[command])
 
-            elif command in ["eq", "gt", "lt"]:
-                self._writeComparisonOperation(command)
+        elif command == "neg":
+            self._writeUnaryOperation("M=-M")
 
-            return None
+        elif command == "not":
+            self._writeUnaryOperation("M=!M")
 
-    def _writeBinaryOperation(operation) -> None:
+        elif command in ["eq", "gt", "lt"]:
+            self._writeComparisonOperation(command)
+
+        return
+
+    def _writeBinaryOperation(self, operation) -> None:
         """
         Ex
             add: M=M+D
         """
         self.file.write(
-                f"// Binary operation: {opertaion}\n"
+                f"// Binary operation: {operation}\n"
                 "@SP\n"
                 "AM=M-1\n"
                 "D=M\n"
                 "A=A-1\n"
                 f"{operation}\n"
                 )
-        return None
+        return
 
-    def _writeUnaryOperation(operation) -> None:
+    def _writeUnaryOperation(self, operation) -> None:
         """
         operation:
             neg, not
         """
         self.file.write(
-                f"// Unary operation: {}\n"
+                f"// Unary operation: {operation}\n"
                 "@SP\n"
                 "A=M-1\n"
                 f"{operation}\n"
                 )
-        return None
+        return
 
-    def _writeComparisonOperation(operation) -> None:
+    def _writeComparisonOperation(self, operation) -> None:
         """
         operation:
             eq, gt, lt
         """
         jump_commands = {
-                "eq": "JMP",
+                "eq": "JEQ",
                 "gt": "JGT",
                 "lt": "JLT",
                 }
         label = self.label_counter
         self.file.write(
-                f"// Comparison operation{operation}\n"
+                f"// Comparison operation {operation}\n"
                 "@SP\n"
                 "AM=M-1\n"
                 "D=M\n" # move to second val(y)
@@ -89,16 +111,16 @@ class CodeWriter:
 
                 # false case
                 "@SP\n"
-                "A=M"
-                "M=0"
-                "@END_{label}\n"
+                "A=M\n"
+                "M=0\n"
+                f"@END_{label}\n"
                 "0;JMP\n"
 
                 # true case
                 f"(TRUE_{label})\n"
                 "@SP\n"
-                "A=M"
-                "M=-1"
+                "A=M\n"
+                "M=-1\n"
 
                 # end
                 f"(END_{label})\n"
@@ -116,31 +138,23 @@ class CodeWriter:
             local, argument, this, that, constant, static,
             temp, pointer
         """
-        segment_table = {
-                "local": "LCL",
-                "argument": "ARG",
-                "this": "THIS",
-                "that": "THAT",
-                "temp": "5", # RAM[5..12]
-                "pointer": 3, # RAM[3..4]
-                }
-        
+                
         if command_type == "C_PUSH":
             if segment == "constant":
                 self.file.write(
                         f"// push constant {index}\n"
-                        "@{index}\n"
+                        f"@{index}\n"
                         "D=A\n"
                         "@SP\n"
                         "A=M\n"
                         "M=D\n"
                         "@SP\n"
-                        "M=M+1"
+                        "M=M+1\n"
                         )
             elif segment == "static":
                 self.file.write(
                         f"// push static {index}\n"
-                        "@{self.current_file}.{index}\n"
+                        f"@{self.current_file}.{index}\n"
                         "D=M\n"
                         "@SP\n"
                         "A=M\n"
@@ -149,25 +163,25 @@ class CodeWriter:
                         "M=M+1\n"
                         )
             elif segment in ["temp", "pointer"]:
-                base = int(self.sement_table[segment])
-                self.file.write(
-                        f"push {segment} {index}\n"
-                        "@{base + index}\n"
-                        "D=M\n"
-                        "@SP\n"
-                        "A=M"
-                        "M=D\n"
-                        "SP\n"
-                        "M=M+1\n"
-                        )
-            else:
-                # local, arument, this, that segment
+                base = int(self.segment_table[segment])
                 self.file.write(
                         f"// push {segment} {index}\n"
-                        "@{segment_table[segment]}\n"
+                        f"@{base + index}\n"
+                        "D=M\n"
+                        "@SP\n"
+                        "A=M\n"
+                        "M=D\n"
+                        "@SP\n"
+                        "M=M+1\n"
+                        )
+            else: # local, argument, this, that segment
+                
+                self.file.write(
+                        f"// push {segment} {index}\n"
+                        f"@{self.segment_table[segment]}\n"
                         "D=M\n" # D = base addr
                         
-                        "@{index}\n"
+                        f"@{index}\n"
                         "A=D+A\n"
                         "D=M\n" # D = RAM[base + idx]
 
@@ -187,23 +201,23 @@ class CodeWriter:
                         "AM=M-1\n"
                         "D=M\n"
 
-                        "@{self.current_file}.{index}\n"
+                        f"@{self.current_file}.{index}\n"
                         "M=D\n"
                         )
             elif segment in ["temp", "pointer"]:
-                base = int(segment_table[segment])
+                base = int(self.segment_table[segment])
                 self.file.write(
                         f"// pop {segment} {index}\n"
                         "@SP\n"
                         "AM=M-1\n"
-                        "D=M"
-                        "@{base + index}\n"
+                        "D=M\n"
+                        f"@{base + index}\n"
                         "M=D\n"
                         )
-            else:
+            else: # local, argument, this, that
                 self.file.write(
                         f"// pop {segment} {index}\n"
-                        "@{segment_table[segment]}\n"
+                        f"@{self.segment_table[segment]}\n"
                         "D=M\n"
                         f"@{index}\n"
                         "D=D+A\n"
@@ -222,10 +236,37 @@ class CodeWriter:
 
 
 
-
 def main():
-    codewriter = CodeWriter()
+    import sys
+    import os
+    from parser import Parser
 
+    if len(sys.argv) != 2:
+        print("Usage: python3 codewrite.py input_file")
+        sys.exit(1)
+
+    input_file = sys.argv[1]
+    output_file = input_file.replace(".vm", ".asm")
+
+    parser = Parser(input_file)
+
+    with CodeWriter(output_file) as writer:
+        file_name = input_file.split("/")[-1].replace(".vm", "")
+        writer.setFileName(file_name)
+
+        while parser.hasMoreCommands():
+            parser.advance()
+            command_type = parser.commandType()
+
+            if command_type == "C_ARITHMETIC":
+                writer.writeArithmetic(parser.arg1())
+            elif command_type in ["C_POP", "C_PUSH"]:
+                writer.writePushPop(
+                        command_type,
+                        parser.arg1(),
+                        int(parser.arg2())
+                        )
+    print("\n\tprogram complete.\n\t")
 
 if __name__ == "__main__":
     main()
